8c6f6430f40bd2188b0f06dcb7ca9c138b2471cf
Leonardo Hirooka <leonardohirooka@hotmail.com>
2023-10-17 21:21:20 -0300

    First Commit

---
+++ frases/lib/main.dart
@@ -0,0 +1,90 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart'; //Temas do App
+import 'dart:math'; //Necessario para gerar numero aleatorio
+
+//Todo o codigo é executado daqui
+void main() {
+  runApp(
+    /* MaterialApp - Classe para montagem de App */
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      /* Remove a faixa de Debug no topo da tela */
+      title: "Frases do Dia",
+      /* home - Pode receber um widget */
+      /* Column - Recebe lista de Widgets chamada children */
+      home: HomeStateful(),
+    ),
+  );
+}
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({super.key});
+
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  var frases = [
+    "Frase 1",
+    "Frase 2",
+    "Frase 3",
+    "Frase 4",
+    "Frase 5",
+  ];
+
+  var fraseGerada = "Pressione para gerar uma frase";
+  void gerarFrase() {
+    var random = Random().nextInt(frases.length); //Gera o numero
+
+//Atualiza "estado" para mostrar as mudanças
+    setState(() {
+      fraseGerada = frases[random];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Frases do Dia"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          /* width: double.infinity, */
+          /* decoration: BoxDecoration(
+            border: Border.all(width: 3, color: Colors.amber),
+          ), */
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("images/logo.png"),
+              Text(
+                fraseGerada,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: gerarFrase,
+                child: const Text(
+                  "Gerar Frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
